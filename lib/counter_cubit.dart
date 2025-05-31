323cf2d6012c2ec36a19ed9e960d894538285a90
Hardik Modha <modha.hardik27@gmail.com>
2025-05-31 20:49:42 +0530

    - Done Cubit example - Done Bloc provider consumer and listener

---
+++ lib/counter_cubit.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'counter_state.dart';
+
+class CounterCubit extends Cubit<CounterState> {
+  CounterCubit() : super(CounterState(counter: 0, isIncremented: false));
+
+  void increment () => emit(CounterState(counter: state.counter + 1, isIncremented: true));
+  void decrement() => emit(CounterState(counter: state.counter - 1, isIncremented: false));
+}
---
+++ lib/counter_state.dart
@@ -0,0 +1,14 @@
+part of 'counter_cubit.dart';
+
+
+
+class CounterState  {
+  int counter;
+  bool isIncremented;
+
+  CounterState({
+    required this.counter,
+    required this.isIncremented
+  });
+
+}
